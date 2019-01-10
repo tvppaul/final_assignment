93beb74edeaea06242918f895ba2959445a8630a
vu0919 <s3749806@rmit.edu.vn>
2019-01-10 09:14:56 +0700

    final_assignment

--- final_assignment.c
+++ final_assignment.c
@@ -7,10 +7,22 @@ char agesorting(int age1, int age2) {
     printf("File cannot be read");
     return 0;
   }
+  FILE *p2 = fopen("temp.txt", "w");
+  if(p2 == NULL) {
+    printf("There was an error");
+    return 0;
+  }
+  // Trying to sort by age
 int age;
-  fscanf(p1, "%i", &age)
+  char c = fgetc(p1);
+  char
+  while(c != EOF) {
+    while(c != 10) {
+    if((c >= (age/10)) && (c <= 57))
+    }
+  }
 }
-
+//coi qsort of stdlib thu di Phuc!Up len github roi mai lam!
 char weightsorting(float weighti, float weightf)
 {
     FILE *p1 = fopen("dataset.txt", "r");
@@ -19,7 +31,15 @@ char weightsorting(float weighti, float weightf)
         printf("File cannot be read\n");
         return -1;
     }
-    if (/* condition */) {
-        /* code */
+
+    FILE *p2 = fopen("temp.txt", "w");
+    if (p2 == NULL)
+    {
+        printf("File cannot be open!\n");
+        return -1;
     }
+
+    float weight;
+
+
 }
